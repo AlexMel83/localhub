622da899b9be6083e91aa22572c1a6c3500ee6b4
Alexander Meleshko <meleshko.a.n@ukr.net>
2026-01-21 23:40:20 +0200

    +

--- app/components/bus/map/markers.vue
+++ app/components/bus/map/markers.vue
@@ -19,20 +19,16 @@ const emit = defineEmits(['marker-click']);
 const busStopsGroup = ref(null);
 const markerClusterGroup = ref(null);
 
-const createIcon = () => {
-  return markRaw(
-    L.divIcon({
-      className: 'custom-bus-stop-marker text-red-500',
-      html: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-        <!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE -->
-        <path fill="currentColor" d="M7 2h10v7.85q0 .575-.25 1.025t-.7.725l-3.55 2.1l.7 2.3H17l-3.1 2.2l1.2 3.8l-3.1-2.35L8.9 22l1.2-3.8L7 16h3.8l.7-2.3l-3.55-2.1q-.45-.275-.7-.725T7 9.85zm4 2v7.05l1 .6l1-.6V4z"/></svg>
-    `,
-      iconSize: [48, 48],
-      iconAnchor: [24, 48],
-      popupAnchor: [0, -48],
-    }),
-  );
-};
+const createIcon = (color = '#f93706') =>
+  L.divIcon({
+    className: 'custom-bus-stop-marker',
+    html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32">
+      <path fill="${color}" d="M22 7v9c0 .71-.38 1.36-1 1.72v1.53c0 .41-.34.75-.75.75h-.5c-.41 0-.75-.34-.75-.75V18h-7v1.25c0 .41-.34.75-.75.75h-.5c-.41 0-.75-.34-.75-.75v-1.53c-.61-.36-1-1.01-1-1.72V7c0-3 3-3 6.5-3S22 4 22 7m-9 8c0-.55-.45-1-1-1s-1 .45-1 1s.45 1 1 1s1-.45 1-1m7 0c0-.55-.45-1-1-1s-1 .45-1 1s.45 1 1 1s1-.45 1-1m0-8h-9v4h9zM7 9.5C6.97 8.12 5.83 7 4.45 7.05A2.5 2.5 0 0 0 2 9.6A2.51 2.51 0 0 0 4 12v8h1v-8c1.18-.24 2-1.29 2-2.5"/>
+    </svg>`,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -28],
+  });
 
 // const createPopupContent = (store) => {
 //   return `
@@ -108,6 +104,11 @@ watch(
 .custom-bus-stop-marker {
   background: transparent;
   border: none;
+}
+
+.leaflet-bus-stop-icon {
+  color: #2563eb; /* blue-600 */
+  filter: drop-shadow(0 3px 6px rgba(0, 0, 0, 0.35));
 }
 
 .custom-bus-stop-marker > div {
